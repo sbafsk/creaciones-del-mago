62339f38b98fa9cfd4b49f0470e24740a3dd1bd7
sbafsk <razor1836@gmail.com>
2025-09-29 13:15:26 -0300

    chore(footer): remove unused Input import

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { MapPin, Mail, Phone, Instagram, Facebook, Twitter } from "lucide-react"
 
